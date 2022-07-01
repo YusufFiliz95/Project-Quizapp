24d9946ad8a9a05162dcbd51c54a2432e904f710
Yusuf Filiz <y.filiz.ch@gmail.com>
2022-07-01 14:09:41 +0200

    added more functions

--- javascript/quiz-questions.js
+++ javascript/quiz-questions.js
@@ -5,6 +5,8 @@ let questions = [
     'answer_2' : 'Die Sonne',
     'answer_3' : 'Ein Weißer Zwerg Supernova',
     'answer_4' : 'Ein Gasriese',
+    'right_answer_text' : 'Ein supermassives schwarzes Loch',
+    'explanation' : 'Im Zentrum der meisten - wenn nicht sogar aller - Galaxien befindet sich nach Ansicht der Astronomen ein supermassereiches Schwarzes Loch. Man schließt das unter anderem aus den Bewegungen der Sterne um das Zentrum der Galaxie, die sich nur durch ein Objekt mit extrem großer Masse auf sehr kleinem Raum erklären lassen: ein Schwarzes Loch. Auch unsere Milchstraße verfügt über eine solche Schwerkraftfalle im Zentrum.',
     'right_answer' : 1
     },
 
@@ -14,24 +16,30 @@ let questions = [
     'answer_2' : 'Hans Lippershey',
     'answer_3' : 'Galileo',
     'answer_4' : 'Johannes Kepler',
+    'right_answer_text' : 'Hans Lippershey',
+    'explanation' : 'Der Erste, der ein Patent für ein Fernrohr anmeldete, war der niederländische Brillenmacher Hans Lippershey (oder Lipperhey). Im Jahr 1608 meldete Lippershey ein Gerät an, das Objekte dreifach vergrößern konnte. Sein Fernrohr hatte ein konkaves Okular, das auf eine konvexe Objektivlinse ausgerichtet war. Eine Geschichte besagt, dass er auf die Idee zu seinem Entwurf kam, nachdem er zwei Kinder in seinem Laden beobachtet hatte, die zwei Linsen hochhielten, die eine weit entfernte Wetterfahne nah erscheinen ließen. Andere behaupteten damals, er habe das Design von einem anderen Brillenmacher, Zacharias Jansen, gestohlen.',
     'right_answer' : 2 
     },
 
     {
-    'question' : 'Welches dieser Objekte ist am weitesten von der Sonne entfernt?',
+    'question' : 'Welcher Planet ist am weitesten von der Sonne entfernt?',
     'answer_1' : 'Merkur',
-    'answer_2' : 'Saturn',
+    'answer_2' : 'Pluto',
     'answer_3' : 'Jupiter',
-    'answer_4' : 'Pluto',
+    'answer_4' : 'Neptun',
+    'right_answer_text' : 'Neptun',
+    'explanation' : 'Der Planet ist rund 4,5 Milliarden Kilometer von der Sonne entfernt. Bis zum August 2006 wäre diese Frage übrigens nicht so eindeutig zu beantworten gewesen. Bis dahin galt nämlich auch Pluto noch als Planet unseres Sonnensystems, inzwischen ist er als Zwergplanet eingestuft. Pluto ist zwischen 4,4 und 7,4 Milliarden Kilometer von der Sonne entfernt. Das bedeutet, dass vor 2006 Pluto der am weitesten von der Sonne entfernte Planet war. Von Januar 1979 bis März 1999 allerdings war Pluto der Sonne näher als Neptun.',
     'right_answer' : 4 
     },
     
     {
-    'question' : 'Welches ist der nach Masse kleinste Planet im Sonnensystem?',
+    'question' : 'Welches ist der kleinste Planet im Sonnensystem?',
     'answer_1' : 'Jupiter',
     'answer_2' : 'Merkur',
-    'answer_3' : 'Erde',
+    'answer_3' : 'Mond',
     'answer_4' : 'Mars',
+    'right_answer_text' : 'Merkur',
+    'explanation' : 'Der kleinste Planet im Sonnensystem ist Merkur, der auch der sonnennächste Planet ist. Mit einem Durchmesser von knapp 4.900 Kilometer ist er deutlich kleiner als der nächstgrößere Planet Mars mit knapp 6.800 Kilometern Durchmesser.',
     'right_answer' : 2 
     },
 
@@ -41,6 +49,8 @@ let questions = [
     'answer_2' : 'Farbe',
     'answer_3' : 'Geruch',
     'answer_4' : 'Größe und Form',
+    'right_answer_text' : 'Größe und Form',
+    'explanation' : 'Planeten können durch ihre große Masse andere Objekte in ihrer Umlaufbahn entweder aufnehmen oder verdrängen. Zwergplaneten sind jedoch wesentlich kleiner und damit nicht in der Lage, eine eigene Laufbahn zu beanspruchen.',
     'right_answer' : 4
     },
 
@@ -50,6 +60,8 @@ let questions = [
     'answer_2' : 'verdrehen und schütteln',
     'answer_3' : 'Umkreisen und drehen',
     'answer_4' : 'Gerade und still',
+    'right_answer_text' : 'Umkreisen und drehen',
+    'explanation' : 'Das 1. keplersche Gesetz beschreibt die Bahnen, auf denen sich Planeten um die Sonne bewegen. Es lautet: Die Planeten bewegen sich auf elliptischen Bahnen. In einem gemeinsamen Brennpunkt steht die Sonne.',
     'right_answer' : 3
     },
 
@@ -59,6 +71,8 @@ let questions = [
     'answer_2' : '9.5 Billionen km',
     'answer_3' : '9.5 Milliarden km',
     'answer_4' : '9.5 Millionen km',
+    'right_answer_text' : '9.5 Billionen km',
+    'explanation' : 'Um die ungeheuren Entfernungen im Weltall beschreiben zu können, benutzt man die Lichtgeschwindigkeit. Die Entfernung eines Lichtjahres ist ca. 9.460.730.472.580,8 Kilometer. Das entspricht gerundet 9,5 Billionen km. Licht legt also in einem Jahr eine Entfernung von rund 9,5 Billionen km zurück.',
     'right_answer' : 2 
     },
 
@@ -68,6 +82,8 @@ let questions = [
     'answer_2' : 'Apollo 13',
     'answer_3' : 'Apollo 17',
     'answer_4' : 'Apollo 7',
+    'right_answer_text' : 'Apollo 11',
+    'explanation' : 'Apollo 11 war die erste bemannte Raumfahrtmission mit einer Mondlandung. Sie war der fünfte bemannte Flug des Apollo-Programms der US-amerikanischen Raumfahrtbehörde NASA. Die Mission verlief erfolgreich und erreichte das 1961 von US-Präsident John F. Kennedy vorgegebene nationale Ziel, noch vor Ende des Jahrzehnts einen Menschen zum Mond und wieder sicher zurück zur Erde zu bringen.',
     'right_answer' : 1 
     },
 
@@ -77,35 +93,108 @@ let questions = [
     'answer_2' : 'Stickstoff',
     'answer_3' : 'Kohlendioxid',
     'answer_4' : 'Sauerstoff',
+    'right_answer_text' : 'Kohlendioxid',
+    'explanation' : 'Der Mars vermag aufgrund seiner geringen Planetenmasse nur eine sehr dünne Atmosphäre zu halten. Der Oberflächendruck beträgt mit 0,006 bar weniger als ein Hundertstel des Atmosphärendrucks auf der Erdoberfläche. Die Marsatmosphäre besteht zu über 95% aus CO_2, knapp 3% aus Stickstoff N_2, und gerade einmal 0,13% entfallen auf Sauerstoff.',
     'right_answer' : 3 
     },
 
     {
     'question' : 'Wie lange braucht das Licht von der Sonne zur Erde?',
     'answer_1' : '10 Jahre',
-    'answer_2' : '8',
+    'answer_2' : '8 Minuten',
     'answer_3' : '1 1/2 Stunden',
     'answer_4' : '7 Monate',
+    'right_answer_text' : '8 Minuten',
+    'explanation' : 'Licht legt in der Sekunde 299.792,458 Kilometer zurück. Die Sonne ist im Mittel 149,60 Millionen km von der Erde entfernt. Das Sonnenlicht benötigt daher etwas mehr als acht Minuten, bis es die Erde erreicht.',
     'right_answer' : 2 
     }
 ];
 
 let currenQuestion = 0;
+let rightAnswers = 0;
 
 function questionAmounts(){
     document.getElementById('quizQuestionAmounts').innerHTML = questions.length;
 }
 
 function showQuestion(){
+
+    if(currenQuestion >= questions.length){
+        document.getElementById('endScreen').classList.remove('hide');
+        document.getElementById('endScreen').classList.add('finish-flex');
+        document.getElementById('questionBody').classList.add('hide');
+        document.getElementById('imgHide').classList.add('hide');
+        document.getElementById('imgShow').classList.remove('hide');
+
+        document.getElementById('allQuestionsAmount').innerHTML = questions.length;
+        document.getElementById('amountOfRightAnswers').innerHTML = rightAnswers;
+    }else{
+
     let question = questions[currenQuestion];
 
+    document.getElementById('questionNumber').innerHTML = currenQuestion + 1;
     document.getElementById('questionText').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
 function answer(selection){
+    let question = questions[currenQuestion]; //Picks the Array
 
+    console.log('Selected answer is', selection); //Picks the array which you clicked on for example: answer_3
+    let selectedQuestionNumber = selection.slice(-1); //After you click an array, it gets the last character of string for exapmple: answer_3 = 3
+    console.log('Selected question number is', selectedQuestionNumber); //Pick the last charackter which you klicked on for example answer_3 = 3
+    console.log('Current question is', question['right_answer']); // It shows which answer is the correct one, the one you defined = 'right_answer' = 2.
+
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if (selectedQuestionNumber == question['right_answer']){ //If selected answer is right: answer_3 = 3 == right_answer = 3.
+        console.log('Richtige Antwort');
+        document.getElementById(selection).parentNode.classList.add('bg-success'); //parentNode = takes the parent element = nimmt den übergeordneten Element
+        document.getElementById('explanation').classList.remove('hide');
+        document.getElementById('explanation').classList.add('d-flex-center');
+        document.getElementById('answer').innerHTML = questions[currenQuestion].right_answer_text;
+        document.getElementById('explanationText').innerHTML = questions[currenQuestion].explanation;
+        rightAnswers++;
+    } else{
+        console.log('Falsche Antwort');
+        document.getElementById(selection).parentNode.classList.add('bg-danger'); //parentNode = takes the parent element = nimmt den übergeordneten Element
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success'); //parentNode = takes the parent element = nimmt den übergeordneten Element
+        document.getElementById('explanation').classList.remove('hide');
+        document.getElementById('explanation').classList.add('d-flex-center');
+        document.getElementById('answer').innerHTML = questions[currenQuestion].right_answer_text;
+        document.getElementById('explanationText').innerHTML = questions[currenQuestion].explanation;
+    }
+    document.getElementById('nextButton').disabled = false; // disables = flase deactivates the disable function
+}
+
+function nextQuestion(){
+    currenQuestion++; //Value increases to 1
+    document.getElementById('nextButton').disabled = true; // disables = true activates the disable function
+    document.getElementById('explanation').classList.remove('d-flex-center');
+    document.getElementById('explanation').classList.add('hide');
+    resetAnswerButtons();
+    showQuestion();
+}
+
+function closeExplanation(){
+    document.getElementById('explanation').classList.toggle('hide');
+}
+
+function doNotClose(event){
+    event.stopPropagation();
+}
+
+function resetAnswerButtons(){
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
