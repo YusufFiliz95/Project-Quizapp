9ec2ebf659eb4ae8f9f82d15f04df57d652ed708
Yusuf Filiz <y.filiz.ch@gmail.com>
2022-07-02 13:14:16 +0200

    added many features

--- javascript/quiz-questions.js
+++ javascript/quiz-questions.js
@@ -1,11 +1,11 @@
 let questions = [
     {
     'question' : 'Was befindet sich im Zentrum einer Galaxie?',
-    'answer_1' : 'Ein supermassives schwarzes Loch',
+    'answer_1' : 'Ein schwarzes Loch',
     'answer_2' : 'Die Sonne',
     'answer_3' : 'Ein Weißer Zwerg Supernova',
     'answer_4' : 'Ein Gasriese',
-    'right_answer_text' : 'Ein supermassives schwarzes Loch',
+    'right_answer_text' : 'Ein schwarzes Loch',
     'explanation' : 'Im Zentrum der meisten - wenn nicht sogar aller - Galaxien befindet sich nach Ansicht der Astronomen ein supermassereiches Schwarzes Loch. Man schließt das unter anderem aus den Bewegungen der Sterne um das Zentrum der Galaxie, die sich nur durch ein Objekt mit extrem großer Masse auf sehr kleinem Raum erklären lassen: ein Schwarzes Loch. Auch unsere Milchstraße verfügt über eine solche Schwerkraftfalle im Zentrum.',
     'right_answer' : 1
     },
@@ -17,8 +17,8 @@ let questions = [
     'answer_3' : 'Galileo',
     'answer_4' : 'Johannes Kepler',
     'right_answer_text' : 'Hans Lippershey',
-    'explanation' : 'Der Erste, der ein Patent für ein Fernrohr anmeldete, war der niederländische Brillenmacher Hans Lippershey (oder Lipperhey). Im Jahr 1608 meldete Lippershey ein Gerät an, das Objekte dreifach vergrößern konnte. Sein Fernrohr hatte ein konkaves Okular, das auf eine konvexe Objektivlinse ausgerichtet war. Eine Geschichte besagt, dass er auf die Idee zu seinem Entwurf kam, nachdem er zwei Kinder in seinem Laden beobachtet hatte, die zwei Linsen hochhielten, die eine weit entfernte Wetterfahne nah erscheinen ließen. Andere behaupteten damals, er habe das Design von einem anderen Brillenmacher, Zacharias Jansen, gestohlen.',
-    'right_answer' : 2 
+    'explanation' : 'Der Erste, der ein Patent für ein Fernrohr anmeldete, war der niederländische Brillenmacher Hans Lippershey (oder Lipperhey). Im Jahr 1608 meldete Lippershey ein Gerät an, das Objekte dreifach vergrößern konnte. Sein Fernrohr hatte ein konkaves Okular, das auf eine konvexe Objektivlinse ausgerichtet war.',
+    'right_answer' : 2
     },
 
     {
@@ -28,8 +28,8 @@ let questions = [
     'answer_3' : 'Jupiter',
     'answer_4' : 'Neptun',
     'right_answer_text' : 'Neptun',
-    'explanation' : 'Der Planet ist rund 4,5 Milliarden Kilometer von der Sonne entfernt. Bis zum August 2006 wäre diese Frage übrigens nicht so eindeutig zu beantworten gewesen. Bis dahin galt nämlich auch Pluto noch als Planet unseres Sonnensystems, inzwischen ist er als Zwergplanet eingestuft. Pluto ist zwischen 4,4 und 7,4 Milliarden Kilometer von der Sonne entfernt. Das bedeutet, dass vor 2006 Pluto der am weitesten von der Sonne entfernte Planet war. Von Januar 1979 bis März 1999 allerdings war Pluto der Sonne näher als Neptun.',
-    'right_answer' : 4 
+    'explanation' : 'Der Planet ist rund 4,5 Milliarden Kilometer von der Sonne entfernt. Bis zum August 2006 wäre diese Frage übrigens nicht so eindeutig zu beantworten gewesen. Bis dahin galt nämlich auch Pluto noch als Planet unseres Sonnensystems, inzwischen ist er als Zwergplanet eingestuft. Pluto ist zwischen 4,4 und 7,4 Milliarden Kilometer von der Sonne entfernt.',
+    'right_answer' : 4
     },
     
     {
@@ -40,7 +40,7 @@ let questions = [
     'answer_4' : 'Mars',
     'right_answer_text' : 'Merkur',
     'explanation' : 'Der kleinste Planet im Sonnensystem ist Merkur, der auch der sonnennächste Planet ist. Mit einem Durchmesser von knapp 4.900 Kilometer ist er deutlich kleiner als der nächstgrößere Planet Mars mit knapp 6.800 Kilometern Durchmesser.',
-    'right_answer' : 2 
+    'right_answer' : 2
     },
 
     {
@@ -73,7 +73,7 @@ let questions = [
     'answer_4' : '9.5 Millionen km',
     'right_answer_text' : '9.5 Billionen km',
     'explanation' : 'Um die ungeheuren Entfernungen im Weltall beschreiben zu können, benutzt man die Lichtgeschwindigkeit. Die Entfernung eines Lichtjahres ist ca. 9.460.730.472.580,8 Kilometer. Das entspricht gerundet 9,5 Billionen km. Licht legt also in einem Jahr eine Entfernung von rund 9,5 Billionen km zurück.',
-    'right_answer' : 2 
+    'right_answer' : 2
     },
 
     {
@@ -84,7 +84,7 @@ let questions = [
     'answer_4' : 'Apollo 7',
     'right_answer_text' : 'Apollo 11',
     'explanation' : 'Apollo 11 war die erste bemannte Raumfahrtmission mit einer Mondlandung. Sie war der fünfte bemannte Flug des Apollo-Programms der US-amerikanischen Raumfahrtbehörde NASA. Die Mission verlief erfolgreich und erreichte das 1961 von US-Präsident John F. Kennedy vorgegebene nationale Ziel, noch vor Ende des Jahrzehnts einen Menschen zum Mond und wieder sicher zurück zur Erde zu bringen.',
-    'right_answer' : 1 
+    'right_answer' : 1
     },
 
     {
@@ -95,7 +95,7 @@ let questions = [
     'answer_4' : 'Sauerstoff',
     'right_answer_text' : 'Kohlendioxid',
     'explanation' : 'Der Mars vermag aufgrund seiner geringen Planetenmasse nur eine sehr dünne Atmosphäre zu halten. Der Oberflächendruck beträgt mit 0,006 bar weniger als ein Hundertstel des Atmosphärendrucks auf der Erdoberfläche. Die Marsatmosphäre besteht zu über 95% aus CO_2, knapp 3% aus Stickstoff N_2, und gerade einmal 0,13% entfallen auf Sauerstoff.',
-    'right_answer' : 3 
+    'right_answer' : 3
     },
 
     {
@@ -106,78 +106,162 @@ let questions = [
     'answer_4' : '7 Monate',
     'right_answer_text' : '8 Minuten',
     'explanation' : 'Licht legt in der Sekunde 299.792,458 Kilometer zurück. Die Sonne ist im Mittel 149,60 Millionen km von der Erde entfernt. Das Sonnenlicht benötigt daher etwas mehr als acht Minuten, bis es die Erde erreicht.',
-    'right_answer' : 2 
-    }
+    'right_answer' : 2
+    },
+    
+    {
+    'question' : 'Welches ist der sonnennächste Planet?',
+    'answer_1' : 'Merkur',
+    'answer_2' : 'Erde',
+    'answer_3' : 'Neptun',
+    'answer_4' : 'Venus',
+    'right_answer_text' : 'Merkur',
+    'explanation' : 'Der Merkur ist mit einem Durchmesser von knapp 4880 Kilometern der kleinste, mit einer durchschnittlichen Sonnenentfernung von etwa 58 Millionen Kilometern der sonnennächste und somit auch schnellste Planet im Sonnensystem.',
+    'right_answer' : 1
+    },
+
+    {
+    'question' : 'Wo liegt der Asteroidengürtel?',
+    'answer_1' : 'Zwischen Erde und Venus',
+    'answer_2' : 'Zwischen Erde und Mars',
+    'answer_3' : 'Zwischen Jupiter und Saturn',
+    'answer_4' : 'Zwischen Mars und Jupiter',
+    'right_answer_text' : 'Zwischen Mars und Jupiter',
+    'explanation' : 'Der Asteroidengürtel, Planetoidengürtel oder Hauptgürtel ist ein Bereich im Sonnensystem mit einer gehäuften Ansammlung von Asteroiden, der sich zwischen den Planetenbahnen von Mars und Jupiter befindet.',
+    'right_answer' : 4
+    },
+
+    {
+    'question' : 'Wo ist der größte Vulkan im Sonnensystem?',
+    'answer_1' : 'Mars',
+    'answer_2' : 'Jupiter',
+    'answer_3' : 'Erde',
+    'answer_4' : 'Venus',
+    'right_answer_text' : 'Mars',
+    'explanation' : 'Der größte Vulkan unseres Sonnensystems, Olympus Mons, ist sehr treffend nach dem Sitz der griechischen Götter benannt worden. Seine Ausmaße sind gigantisch. Er ist – bei einem Basisdurchmesser von 600 km - 22 km hoch! Demgegenüber erscheint der höchste Berg der Erde, der Mount Everest, mit seinen knapp 9 km geradezu als Winzling.',
+    'right_answer' : 1
+    },
+
+    {
+    'question' : 'Welche der folgenden Aussagen beschreibt die Atmosphäre der Venus am besten?',
+    'answer_1' : 'Kalt und verschneit',
+    'answer_2' : 'Heiß und giftig',
+    'answer_3' : 'Hell und sonnig',
+    'answer_4' : 'Kalt und nass',
+    'right_answer_text' : 'Heiß und giftig',
+    'explanation' : 'Die Venus ist von dicken, säurehaltigen Wolken umgeben, die sich etwa 50 km über ihrer Oberfläche befinden',
+    'right_answer' : 2
+    },
+
+    {
+    'question' : 'Was ist der große rote Fleck auf dem Jupiter?',
+    'answer_1' : 'Ein Sturm',
+    'answer_2' : 'Ein Krater',
+    'answer_3' : 'Ein Vulkan',
+    'answer_4' : 'Ein See',
+    'right_answer_text' : 'Ein Sturm',
+    'explanation' : 'Der Große Rote Fleck des Jupiters ist der Megasturm des Sonnensystems: Mit einem Durchmesser von rund 16 000 Kilometern tobt er seit mindestens 200 Jahren über den Gasriesen – die Erde fände in dem Gebilde Platz.',
+    'right_answer' : 1
+    },
 ];
 
 let currenQuestion = 0;
 let rightAnswers = 0;
+let AUDIO_CORRECT = new Audio('sounds/correct.mp3');
+    AUDIO_CORRECT.volume = 0.2;
+let AUDIO_INCORRECT = new Audio('sounds/incorrect.mp3');
+    AUDIO_INCORRECT.volume = 0.3;
+
 
 function questionAmounts(){
     document.getElementById('quizQuestionAmounts').innerHTML = questions.length;
 }
 
 function showQuestion(){
-
-    if(currenQuestion >= questions.length){
-        document.getElementById('endScreen').classList.remove('hide');
-        document.getElementById('endScreen').classList.add('finish-flex');
-        document.getElementById('questionBody').classList.add('hide');
-        document.getElementById('imgHide').classList.add('hide');
-        document.getElementById('imgShow').classList.remove('hide');
-
-        document.getElementById('allQuestionsAmount').innerHTML = questions.length;
-        document.getElementById('amountOfRightAnswers').innerHTML = rightAnswers;
+    if(gameIsOver()){
+        showEndScreen();
     }else{
+        updateProgressBar();
+        showNextQuestion();
+    }
+}
 
+function gameIsOver(){
+    currenQuestion >= questions.length
+}
+
+function showEndScreen(){
+    document.getElementById('endScreen').classList.remove('hide');
+    document.getElementById('endScreen').classList.add('finish-flex');
+    document.getElementById('questionBody').classList.add('hide');
+    document.getElementById('imgHide').classList.add('hide');
+    document.getElementById('imgShow').classList.remove('hide');
+    document.getElementById('allQuestionsAmount').innerHTML = questions.length;
+    document.getElementById('amountOfRightAnswers').innerHTML = rightAnswers;
+}
+
+function showNextQuestion(){
+    updateProgressBar();
     let question = questions[currenQuestion];
-
     document.getElementById('questionNumber').innerHTML = currenQuestion + 1;
     document.getElementById('questionText').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
-    }
+}
+
+function updateProgressBar(){
+    let percent = (currenQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progressBar').style.width = `${percent}%`;
 }
 
 function answer(selection){
-    let question = questions[currenQuestion]; //Picks the Array
-
-    console.log('Selected answer is', selection); //Picks the array which you clicked on for example: answer_3
-    let selectedQuestionNumber = selection.slice(-1); //After you click an array, it gets the last character of string for exapmple: answer_3 = 3
-    console.log('Selected question number is', selectedQuestionNumber); //Pick the last charackter which you klicked on for example answer_3 = 3
-    console.log('Current question is', question['right_answer']); // It shows which answer is the correct one, the one you defined = 'right_answer' = 2.
-
+    let question = questions[currenQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-    if (selectedQuestionNumber == question['right_answer']){ //If selected answer is right: answer_3 = 3 == right_answer = 3.
-        console.log('Richtige Antwort');
-        document.getElementById(selection).parentNode.classList.add('bg-success'); //parentNode = takes the parent element = nimmt den übergeordneten Element
-        document.getElementById('explanation').classList.remove('hide');
-        document.getElementById('explanation').classList.add('d-flex-center');
-        document.getElementById('answer').innerHTML = questions[currenQuestion].right_answer_text;
-        document.getElementById('explanationText').innerHTML = questions[currenQuestion].explanation;
+    if (selectedQuestionNumber == question['right_answer']){ 
+        document.getElementById(selection).parentNode.classList.add('bg-success'); 
+        setTimeout(showExplanation, 2000);
+        disableButtons();
+        AUDIO_CORRECT.play();
         rightAnswers++;
     } else{
-        console.log('Falsche Antwort');
-        document.getElementById(selection).parentNode.classList.add('bg-danger'); //parentNode = takes the parent element = nimmt den übergeordneten Element
-        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success'); //parentNode = takes the parent element = nimmt den übergeordneten Element
-        document.getElementById('explanation').classList.remove('hide');
-        document.getElementById('explanation').classList.add('d-flex-center');
-        document.getElementById('answer').innerHTML = questions[currenQuestion].right_answer_text;
-        document.getElementById('explanationText').innerHTML = questions[currenQuestion].explanation;
+        document.getElementById(selection).parentNode.classList.add('bg-danger'); 
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success'); 
+        setTimeout(showExplanation, 2000);
+        disableButtons();
+        AUDIO_INCORRECT.play();
     }
-    document.getElementById('nextButton').disabled = false; // disables = flase deactivates the disable function
+        setTimeout(disableButton, 2000)
+}
+
+function disableButtons(){
+    document.getElementById('disableDiv_1').classList.add('disable-div');
+    document.getElementById('disableDiv_2').classList.add('disable-div');
+    document.getElementById('disableDiv_3').classList.add('disable-div');
+    document.getElementById('disableDiv_4').classList.add('disable-div');
 }
 
 function nextQuestion(){
-    currenQuestion++; //Value increases to 1
-    document.getElementById('nextButton').disabled = true; // disables = true activates the disable function
+    document.getElementById('infoExplanation').classList.add('hide');
+    document.getElementById('infoExplanation').classList.remove('d-flex-center');
+    enableButtons();
+    currenQuestion++;
+    document.getElementById('nextButton').disabled = true;
     document.getElementById('explanation').classList.remove('d-flex-center');
     document.getElementById('explanation').classList.add('hide');
     resetAnswerButtons();
     showQuestion();
+}
+
+function enableButtons(){
+    document.getElementById('disableDiv_1').classList.remove('disable-div');
+    document.getElementById('disableDiv_2').classList.remove('disable-div');
+    document.getElementById('disableDiv_3').classList.remove('disable-div');
+    document.getElementById('disableDiv_4').classList.remove('disable-div');
 }
 
 function closeExplanation(){
@@ -197,4 +281,28 @@ function resetAnswerButtons(){
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function restartGame(){
+    document.getElementById('endScreen').classList.add('hide');
+    document.getElementById('endScreen').classList.remove('finish-flex');
+    document.getElementById('questionBody').classList.remove('hide');
+    document.getElementById('imgHide').classList.remove('hide');
+    document.getElementById('imgShow').classList.add('hide');
+    currenQuestion = 0;
+    rightAnswers = 0;
+    init();
+}
+
+function showExplanation(){
+    document.getElementById('explanation').classList.remove('hide');
+    document.getElementById('explanation').classList.add('d-flex-center');
+    document.getElementById('answer').innerHTML = questions[currenQuestion].right_answer_text;
+    document.getElementById('explanationText').innerHTML = questions[currenQuestion].explanation;
+    document.getElementById('infoExplanation').classList.remove('hide');
+    document.getElementById('infoExplanation').classList.add('d-flex-center');
+}
+
+function disableButton(){
+    document.getElementById('nextButton').disabled = false;
 }
